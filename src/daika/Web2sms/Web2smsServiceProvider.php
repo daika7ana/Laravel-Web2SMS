4b2f436679109bfcedc449dd05cd662036bfece2
daika7ana <trol4ever@gmail.com>
2017-08-07 17:31:50 +0300

    Create Web2smsServiceProvider.php

---
+++ src/daika/Web2sms/Web2smsServiceProvider.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace Daika\Web2sms;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\AliasLoader;
+
+class Web2smsServiceProvider extends ServiceProvider 
+{
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
+  public function boot() {
+    $this->publishes([
+        __DIR__.'/../../config/config.php' => config_path('web2sms.php'),
+    ], 'config');
+    AliasLoader::getInstance()->alias('Web2sms', 'Daika\Web2sms\SmsSender');
+  }
+  
+  public function register() {
+    $this->app->singleton(SmsSender::class);
+  }
+}
